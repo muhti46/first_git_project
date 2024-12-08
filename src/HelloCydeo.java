b7a12df330535d04629823714fa67f0140f48c69
muhti46 <muhteremdemir@gmail.com>
2024-12-08 21:05:30 +0100

    Update HelloCydeo.java

--- src/HelloCydeo.java
+++ src/HelloCydeo.java
@@ -2,6 +2,13 @@ public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
 
-        //SOme changes are added.
+        //Some changes are added.
+        int a=25;
+        int b=15;
+        int c;
+        if(a>b)
+          c=a-b;
+        else
+            b-a;
     }
 }
