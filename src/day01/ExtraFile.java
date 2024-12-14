1c71e204f1a140435c115c20b0658382c44ea9a6
muhti46 <muhteremdemir@gmail.com>
2024-12-14 23:31:59 +0100

    Ok

--- src/day01/ExtraFile.java
+++ src/day01/ExtraFile.java
@@ -1,7 +1,9 @@
 package day01;
 
 public class ExtraFile {
+    public static void main(String[] args) {
+        // this is an extra file
+        System.out.println("Extra File is uploaded");
+    }
 
-    // this is an extra file
-    System.out.println("Extra File is uploaded");
 }
