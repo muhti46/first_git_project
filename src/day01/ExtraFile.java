a6cfadcd07349e6694ae204bd52bf9e4a127d3e2
muhti46 <muhteremdemir@gmail.com>
2024-12-14 23:33:25 +0100

    Update ExtraFile.java

--- src/day01/ExtraFile.java
+++ src/day01/ExtraFile.java
@@ -4,6 +4,9 @@ public class ExtraFile {
     public static void main(String[] args) {
         // this is an extra file
         System.out.println("Extra File is uploaded");
+        int a=5;
+        int b=15;
+        int c=a+b;
     }
 
 }
